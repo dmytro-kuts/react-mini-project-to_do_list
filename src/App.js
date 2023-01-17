3d76d0df8051026719cdee67fd130ac2fecb47db
Dmytro24 <dmytro240189@gmail.com>
2023-01-17 18:28:39 +0200

    created new app

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import { ListItem } from "./components/ListItem";
+import { TaskField } from "./components/TaskField";
+
+function App() {
+    const [tasks, setTasks] = useState([
+        {
+            text: "Вивчити ReactJS",
+            completed: true,
+        },
+        {
+            text: "Розробити ToDo на ReactJS",
+            completed: false,
+        },
+    ]);
+
+    const onToggleCompleted = (index) => {
+        setTasks((prevTasks) =>
+            prevTasks.map((task, curIdx) =>
+                index === curIdx
+                    ? {
+                          ...task,
+                          completed: !task.completed,
+                      }
+                    : task
+            )
+        );
+    };
+
+    const onRemoveTask = (index) => {
+        setTasks((prevTasks) =>
+            prevTasks.filter((_, curIdx) => index !== curIdx)
+        );
+    };
+
+    const onAddTask = (text) => {
+        setTasks((prevTasks) => [
+            ...prevTasks,
+            {
+                text,
+                completed: false,
+            },
+        ]);
+    };
+
+    return (
+        <div className="todo">
+            <div className="todo__header">
+                <h4>Список задач</h4>
+            </div>
+            <TaskField onAddTask={onAddTask} />
+            <div className="todo__list">
+                {tasks.map((task, index) => (
+                    <ListItem
+                        key={index}
+                        index={index}
+                        text={task.text}
+                        completed={task.completed}
+                        onToggleCompleted={onToggleCompleted}
+                        onRemoveTask={onRemoveTask}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default App;
